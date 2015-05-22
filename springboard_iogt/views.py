4dabac44a463e885571db1c4799c452dd651bfbd
Rizmari Versfeld <rizziepit@gmail.com>
2015-05-22 17:43:56 +0200

    override home view to add recent_content to context

---
+++ springboard_iogt/views.py
@@ -0,0 +1,60 @@
+from random import randint, shuffle
+
+from pyramid.view import view_config
+from beaker.cache import cache_region
+from elasticutils import F
+
+from springboard.views.base import SpringboardViews
+
+
+HOUR_CACHE_REGION = 'hour'
+
+
+class IoGTViews(SpringboardViews):
+
+    @view_config(route_name='home',
+                 renderer='springboard_iogt:templates/home.jinja2')
+    def index_view(self):
+        return self.context(
+            recent_content=self.recent_content())
+
+    @cache_region(HOUR_CACHE_REGION)
+    def _recent_content(self, language, limit):
+        categories = self.all_categories.filter(
+            language=language).everything()
+
+        def get_category_object(uuid):
+            if not uuid:
+                return None
+            [category] = filter(lambda c: c.uuid == uuid, categories)
+            return category
+
+        # get 2 most recent pages
+        [page1, page2] = self.all_pages.filter(
+            language=self.language).order_by('-created_at')[:2]
+        category1 = get_category_object(page1.primary_category)
+        category2 = get_category_object(page2.primary_category)
+
+        # get most recent page per category
+        # exclude the 2 most recent overall
+        content = []
+        f = ~F(uuid=page1.uuid) & ~F(uuid=page2.uuid)
+        for category in categories:
+            try:
+                f_cat = F(primary_category=category.uuid)
+                [page] = self.all_pages.filter(f & f_cat).order_by(
+                    '-created_at')[:1]
+                content.append((category, page))
+            except ValueError:
+                pass
+
+        shuffle(content)
+        content = content[:limit - 2]
+        content.insert(randint(0, len(content)), (category1, page1))
+        content.insert(randint(0, len(content)), (category2, page2))
+        return content
+
+    def recent_content(self, limit=8):
+        content = self._recent_content(self.language, limit)
+        content = [(c.to_object(), p.to_object()) for c, p in content]
+        return content
