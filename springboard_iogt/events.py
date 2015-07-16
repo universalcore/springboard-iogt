df44bbd7cd7a5d51244d3a3c43b7bac387806a8b
Rizmari Versfeld <rizziepit@gmail.com>
2015-07-16 14:25:53 +0200

    clean up

--- springboard_iogt/events.py
+++ springboard_iogt/events.py
@@ -6,8 +6,6 @@ from springboard_iogt.utils import ContentSection
 @subscriber(BeforeRender)
 def add_content_section_context(event):
     index_prefixes = getattr(event.get('view'), 'all_index_prefixes', None)
-    if not index_prefixes:
-        event['content_sections'] = []
-        return
-
-    event['content_sections'] = ContentSection.active(index_prefixes)
+    event['content_sections'] = (ContentSection.active(index_prefixes)
+                                 if index_prefixes
+                                 else [])
